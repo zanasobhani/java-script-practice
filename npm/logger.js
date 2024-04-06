e21716495daa100d1edd925069e4d8cf5b8b11c0
Zana <zanasobhani@gmail.com>
2024-04-06 18:57:50 +0200

    Solution of exercise on npm.

---
+++ npm/logger.js
@@ -0,0 +1,14 @@
+import chalk from "chalk";
+
+
+
+export function logRed(text) {
+    return chalk.red(text);
+}
+export function logBlue(text) {
+    return chalk.blue(text);
+}
+export function logGreen(text) {
+    return chalk.green(text);
+}
+
